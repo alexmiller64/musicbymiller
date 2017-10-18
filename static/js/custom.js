e848b6e31d71455441c4a069ee7428e65a7fa5e9
Alex Miller <alexander.dean.miller@gmail.com>
2017-10-18 16:43:01 +0100

    adding js and extra functionality

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,9 @@
-function ep() {
-    document.getElementById("test").innerHTML = "this.src='/static/images/stage.jpg";
-}
+$() .jQuery(function(){
 
-function ep2() {
-  document.getElementById("test").innerHTML = "/static/images/park.jpg";
-}
+   // Fade In
+   $("#myImage").fadeIn('slow');
+
+   // // Fade Out
+   // $("#myImage").fadeOut();
+
+});
