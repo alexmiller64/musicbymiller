0a420efd3de21c20bc5b2174a8868efbb2391420
Alex Miller <alexander.dean.miller@gmail.com>
2017-10-18 20:22:27 +0100

    adding some JS visuals

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,81 @@
-$() .jQuery(function(){
+$(function(){
 
    // Fade In
-   $("#myImage").fadeIn('slow');
+   $("#myImage1").hide().fadeIn(1500);
+
+   // // Fade Out
+   // $("#myImage1")onmouseout.fadeOut();
+
+
+
+});
+
+$(function(){
+
+   // Fade In
+   $(".myImage1").hide().fadeIn(1500);
+
+   // // Fade Out
+   // $("#myImage1")onmouseout.fadeOut();
+
+
+
+});
+
+$(function(){
+
+   // Fade In
+   $("#myImage2").hide().fadeIn(2500);
+
+   // // Fade Out
+   // $("#myImage").fadeOut();
+
+});
+
+$(function(){
+
+   // Fade In
+   $(".myImage2").hide().fadeIn(2500);
+
+   // // Fade Out
+   // $("#myImage").fadeOut();
+
+});
+
+$(function(){
+
+   // Fade In
+   $("#myImage3").hide().fadeIn(3500);
+
+   // // Fade Out
+   // $("#myImage").fadeOut();
+
+});
+
+$(function(){
+
+   // Fade In
+   $(".myImage3").hide().fadeIn(3500);
+
+   // // Fade Out
+   // $("#myImage").fadeOut();
+
+});
+
+$(function(){
+
+   // Fade In
+   $(".myImage4").hide().fadeIn(4000);
+
+   // // Fade Out
+   // $("#myImage").fadeOut();
+
+});
+
+$(function(){
+
+   // Fade In
+   $("#scroll2").hide().fadeIn(4000);
 
    // // Fade Out
    // $("#myImage").fadeOut();
